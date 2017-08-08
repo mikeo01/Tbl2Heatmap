09a001980af731cceb1acca9c26ed9b56cca5398
Michael Long <longy08@hotmail.co.uk>
2017-08-08 15:58:22 +0100

    Add files via upload

---
+++ tbl2heatmap.js
@@ -0,0 +1,103 @@
+var Heatmap;
+(function (Heatmap) {
+    // Default values
+    var SATURATION = 80;
+    var LIGHTNESS = 50;
+    /**
+     * Static hues for use with heatmap (default colours)
+     */
+    var Hues;
+    (function (Hues) {
+        Hues[Hues["Red"] = 0] = "Red";
+        Hues[Hues["Orange"] = 30] = "Orange";
+        Hues[Hues["Yellow"] = 60] = "Yellow";
+        Hues[Hues["Green"] = 90] = "Green";
+        Hues[Hues["Turquoise"] = 180] = "Turquoise";
+        Hues[Hues["Blue"] = 240] = "Blue";
+        Hues[Hues["Purple"] = 260] = "Purple";
+        Hues[Hues["Pink"] = 300] = "Pink";
+    })(Hues || (Hues = {}));
+    /**
+     * Colors is for use externally if a default HSL is desired
+     */
+    var Colors = (function () {
+        function Colors() {
+        }
+        Colors.Red = { h: Hues.Red, s: SATURATION, l: LIGHTNESS };
+        Colors.Orange = { h: Hues.Orange, s: SATURATION, l: LIGHTNESS };
+        Colors.Yellow = { h: Hues.Yellow, s: SATURATION, l: LIGHTNESS };
+        Colors.Green = { h: Hues.Green, s: SATURATION, l: LIGHTNESS };
+        Colors.Turquoise = { h: Hues.Turquoise, s: SATURATION, l: LIGHTNESS };
+        Colors.Blue = { h: Hues.Blue, s: SATURATION, l: LIGHTNESS };
+        Colors.Purple = { h: Hues.Purple, s: SATURATION, l: LIGHTNESS };
+        Colors.Pink = { h: Hues.Pink, s: SATURATION, l: LIGHTNESS };
+        /**
+         * Helper method to generate a HSL
+         */
+        Colors.generateHSL = function (h, s, l) {
+            return {
+                h: h,
+                s: s,
+                l: l
+            };
+        };
+        /**
+         * Helper method to output HSL string
+         */
+        Colors.output = function (hsl) {
+            return "hsl(" + hsl.h + ", " + hsl.s + "%, " + hsl.l + "%)";
+        };
+        return Colors;
+    }());
+    Heatmap.Colors = Colors;
+    /**
+     * generateHeatmap builds a heatmap from tabular data
+     * @param h IHeatmap    Interface required for heatmap generation
+     */
+    function generateHeatmap(h) {
+        try {
+            var hsl = h.getShade();
+            var cells = Array.prototype.slice.call(h.getTable().querySelectorAll('.heatmap tr .heatmap-cell'));
+            // For dynamic heatmap
+            var heatmap = {};
+            // Build up heatmap information
+            for (var _i = 0, cells_1 = cells; _i < cells_1.length; _i++) {
+                var cell = cells_1[_i];
+                // Clear any characters which shouldn't be there
+                var val = String(parseInt(cell.innerHTML));
+                // Initialise empty
+                if (typeof heatmap[val] == 'undefined') {
+                    heatmap[val] = {
+                        hsl: null,
+                        htmlElements: []
+                    };
+                }
+                // Store cell which uses this particular entry
+                heatmap[val].htmlElements.push(cell);
+            }
+            // Calculate range
+            var range = 100 - hsl.l;
+            // Calculate spread
+            var spread = range / Object.keys(heatmap).length;
+            // Calculate heatmap length
+            var x = Object.keys(heatmap).length;
+            // Calculate HSL and apply background colours
+            for (var h_1 in heatmap) {
+                // Calculate HSL
+                heatmap[h_1].hsl = Colors.generateHSL(hsl.h, hsl.s, (spread * x) + range);
+                // Apply background colours
+                for (var _a = 0, _b = heatmap[h_1].htmlElements; _a < _b.length; _a++) {
+                    var cell = _b[_a];
+                    cell.style.backgroundColor = Colors.output(heatmap[h_1].hsl);
+                }
+                --x;
+            }
+            return true;
+        }
+        catch (e) {
+            console.error(e);
+            return false;
+        }
+    }
+    Heatmap.generateHeatmap = generateHeatmap;
+})(Heatmap || (Heatmap = {}));
---
+++ tbl2heatmap.ts
@@ -0,0 +1,155 @@
+/**
+* PDFMake Facade.
+* @link https://github.com/mikeo01/pdfmake_facade The PDFMake_Facade GitHub project
+* @author Michael Long <support@mv-xfusion.co.uk>
+* @license http://opensource.org/licenses/MIT The MIT License (MIT)
+* @note This program is distributed in the hope that it will be useful - WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+namespace Heatmap {
+    // Default values
+    const SATURATION: number = 80;
+    const LIGHTNESS: number = 50;
+
+    /**
+     * HSL defines HSL object required for Hue class
+     */
+    interface HSL {
+        h: number;
+        s: number;
+        l: number;
+    }
+
+    /**
+     * Static hues for use with heatmap (default colours)
+     */
+    enum Hues {
+        Red = 0,
+        Orange = 30,
+        Yellow = 60,
+        Green = 90,
+        Turquoise = 180,
+        Blue = 240,
+        Purple = 260,
+        Pink = 300,
+    }
+
+    /**
+     * Colors is for use externally if a default HSL is desired
+     */
+    export class Colors {
+        static readonly Red: HSL = { h: Hues.Red, s: SATURATION, l: LIGHTNESS };
+        static readonly Orange: HSL = { h: Hues.Orange, s: SATURATION, l: LIGHTNESS };
+        static readonly Yellow: HSL = { h: Hues.Yellow, s: SATURATION, l: LIGHTNESS };
+        static readonly Green: HSL = { h: Hues.Green, s: SATURATION, l: LIGHTNESS };
+        static readonly Turquoise: HSL = { h: Hues.Turquoise, s: SATURATION, l: LIGHTNESS };
+        static readonly Blue: HSL = { h: Hues.Blue, s: SATURATION, l: LIGHTNESS };
+        static readonly Purple: HSL = { h: Hues.Purple, s: SATURATION, l: LIGHTNESS };
+        static readonly Pink: HSL = { h: Hues.Pink, s: SATURATION, l: LIGHTNESS };
+        /**
+         * Helper method to generate a HSL
+         */
+        static readonly generateHSL = function (h: number, s: number, l: number): HSL {
+            return {
+                h: h,
+                s: s,
+                l: l
+            };
+        };
+        /**
+         * Helper method to output HSL string
+         */
+        static readonly output = function (hsl: HSL): string {
+            return `hsl(` + hsl.h + `, ` + hsl.s + `%, ` + hsl.l + `%)`;
+        };
+    }
+
+    /**
+     * IHeatmap describes the methods required for heatmap generation
+     */
+    export interface IHeatmap {
+        /**
+         * getShade should return a string a HSL shade for heatmap use
+         */
+        getShade(): HSL;
+        /**
+         * getTable should return the table HTMLElement to convert to heatmap
+         */
+        getTable(): HTMLElement;
+    }
+
+    /**
+     * Generic interface which describes heatmap information
+     */
+    interface Heatmap {
+        hsl: HSL | null; // Shade to use (nullable)
+        htmlElements: Array<HTMLElement>; // Elements which use this shade
+    }
+
+    /**
+     * Dictionary types that is described by Heatmap
+     */
+    type Dictionary = { [key: string]: Heatmap };
+
+    /**
+     * generateHeatmap builds a heatmap from tabular data
+     * @param h IHeatmap    Interface required for heatmap generation
+     */
+    export function generateHeatmap(h: IHeatmap): boolean {
+        try {
+            const hsl: HSL = h.getShade();
+            const cells: any = Array.prototype.slice.call(
+                h.getTable().querySelectorAll('.heatmap .heatmap-row .heatmap-cell')
+            );
+
+            // For dynamic heatmap
+            let heatmap: Dictionary = {};
+
+            // Build up heatmap information
+            for (const cell of cells) {
+                // Clear any characters which shouldn't be there
+                const val = String(parseInt(cell.innerHTML));
+
+                // Initialise empty
+                if (typeof heatmap[val] == 'undefined') {
+                    heatmap[val] = {
+                        hsl: null,
+                        htmlElements: []
+                    };
+                }
+
+                // Store cell which uses this particular entry
+                heatmap[val].htmlElements.push(cell);
+            }
+
+            // Calculate range
+            const range: number = 100 - hsl.l;
+            // Calculate spread
+            const spread: number = range / Object.keys(heatmap).length;
+            // Calculate heatmap length
+            let x = Object.keys(heatmap).length;
+
+            // Calculate HSL and apply background colours
+            for (let h in heatmap) {
+                // Calculate HSL
+                heatmap[h].hsl = Colors.generateHSL(hsl.h, hsl.s, (spread * x) + range);
+
+                // Apply background colours
+                for (const cell of heatmap[h].htmlElements) {
+                    cell.style.backgroundColor = Colors.output(
+                        heatmap[h].hsl
+                    );
+                }
+
+                --x;
+            }
+
+            return true;
+        } catch (e) {
+            console.error(e);
+            return false;
+        }
+    }
+}
